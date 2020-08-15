e5e86f33dc36cd57a4b0f9dd3452a84517f50b5b
Everton <ton.oliveira@sempre.facig.edu.br>
2020-08-15 14:09:58 -0300

    Deixa um prato insdisponível

--- src/components/Food/index.tsx
+++ src/components/Food/index.tsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import { FiEdit3, FiTrash } from 'react-icons/fi';
 
 import { Container } from './styles';
+import api from '../../services/api';
 
 interface IFoodPlate {
   id: number;
@@ -28,6 +29,16 @@ const Food: React.FC<IProps> = ({
 
   async function toggleAvailable(): Promise<void> {
     // TODO UPDATE STATUS (available)
+    try {
+      await api.put(`/foods/${food.id}`, {
+        ...food,
+        available: !isAvailable,
+      });
+
+      setIsAvailable(!isAvailable);
+    } catch (err) {
+      console.log(err);
+    }
   }
 
   function setEditingFood(): void {
